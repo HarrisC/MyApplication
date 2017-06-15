bd23eaf3498cc27992906088792ca96e7dc9a9f8
Harris <harris@cyberoffice.hk>
2017-06-15 11:01:27 +0800

    fixed listview scroll

--- app/src/main/java/com/example/inspiron/myapplication/Fragment1.java
+++ app/src/main/java/com/example/inspiron/myapplication/Fragment1.java
@@ -22,6 +22,7 @@ public class Fragment1 extends Fragment {
     static String[] name;
     private static ListView listView;
     private static List<ItemModel> list;
+    private View view;
 
     private int mPage;
 
@@ -43,9 +44,7 @@ public class Fragment1 extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_page1, container, false);
-        //TextView textView = (TextView) view;
-        //textView.setText("Fragment #" + mPage);
+        view = inflater.inflate(R.layout.fragment_page1, container, false);
 
         list = new ArrayList<>();
         image = view.getResources().obtainTypedArray(R.array.image_set);
